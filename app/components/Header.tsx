3454c0150a2288eb0186f3240c9955da8bb7be09
Guo Yunhe <i@guoyunhe.me>
2023-11-05 23:12:34 +0800

    header link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,7 @@ import {Suspense} from 'react';
 import type {HeaderQuery} from 'storefrontapi.generated';
 import type {LayoutProps} from './Layout';
 import {useRootLoaderData} from '~/root';
+import {Box, SvgIcon, Typography} from '@mui/joy';
 
 type HeaderProps = Pick<LayoutProps, 'header' | 'cart' | 'isLoggedIn'>;
 
@@ -11,17 +12,61 @@ type Viewport = 'desktop' | 'mobile';
 export function Header({header, isLoggedIn, cart}: HeaderProps) {
   const {shop, menu} = header;
   return (
-    <header className="header">
-      <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
-        <strong>{shop.name}</strong>
-      </NavLink>
+    <Box
+      component="header"
+      sx={{
+        height: 44,
+        backdropFilter: 'saturate(180%) blur(20px)',
+        background: 'rgba(22, 22, 23, 0.8)',
+        position: 'fixed',
+        zIndex: 100,
+        top: 0,
+        left: 0,
+        right: 0,
+        accentColor: 'auto',
+        color: 'rgba(255, 255, 255, 0.7)',
+        px: 2,
+        alignItems: 'center',
+        display: 'flex',
+        userSelect: 'none',
+      }}
+    >
+      <Typography
+        component={NavLink}
+        prefetch="intent"
+        to="/"
+        end
+        sx={{
+          color: 'inherit',
+          transition: 'color .32s cubic-bezier(.4,0,.6,1)',
+          '&:hover': {
+            color: '#ffffff',
+          },
+        }}
+      >
+        <svg
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+          style={{
+            width: '1em',
+            height: '1em',
+            verticalAlign: -2,
+            marginRight: '0.5em',
+          }}
+        >
+          <path d="M14 13.292A8 8 0 0 0 8.5.015v7.778l5.5 5.5zm-.708.708L8.5 9.206v6.778a7.967 7.967 0 0 0 4.792-1.986zM7.5 15.985V9.207L2.708 14A7.967 7.967 0 0 0 7.5 15.985zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5z" />
+        </svg>
+        {shop.name}
+      </Typography>
       <HeaderMenu
         menu={menu}
         viewport="desktop"
         primaryDomainUrl={header.shop.primaryDomain.url}
       />
       <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
-    </header>
+    </Box>
   );
 }
 
@@ -68,17 +113,23 @@ export function HeaderMenu({
             ? new URL(item.url).pathname
             : item.url;
         return (
-          <NavLink
-            className="header-menu-item"
-            end
+          <Typography
             key={item.id}
-            onClick={closeAside}
+            component={NavLink}
             prefetch="intent"
-            style={activeLinkStyle}
             to={url}
+            end
+            onClick={closeAside}
+            sx={{
+              color: 'inherit',
+              transition: 'color .32s cubic-bezier(.4,0,.6,1)',
+              '&:hover': {
+                color: '#ffffff',
+              },
+            }}
           >
             {item.title}
-          </NavLink>
+          </Typography>
         );
       })}
     </nav>
