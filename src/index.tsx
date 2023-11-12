8b989a4e2941c3a8d4c29cc08c7161b099d236e3
Guo Yunhe <i@guoyunhe.me>
2023-11-12 22:49:03 +0800

    adsense

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import '~/bootstrap/i18n';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '~/App';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
